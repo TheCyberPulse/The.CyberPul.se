aa895fcbf79c7ca115a1d4dbaaeed0e02b22ba35
XERAEN <x@XERAEN.net>
2022-07-06 20:23:41 -0500

    Refactors code for default redirect

--- hackr.tv.rb
+++ hackr.tv.rb
@@ -1,10 +1,25 @@
 require 'sinatra'
 require 'yaml'
 
-link_dictionary = YAML.load_file('link_dictionary.yml')
+FINAL_DEFAULT_URL = 'https://the.cyberpul.se'
 
 get '/:shortcode' do
-  new_link = link_dictionary[params['shortcode']].to_s
-  redirect new_link if new_link.length > 0
-  redirect 'https://the.cyberpul.se' unless new_link.length > 0
+  redirect resolve_link(params['shortcode'].to_s.downcase)
+end
+
+get '/:shorcode/*' do
+  redirect resolve_link(params['shortcode'].to_s.downcase)
+end
+
+get '/' do
+  redirect resolve_link('')
+end
+
+def resolve_link(code)
+  link_dictionary = YAML.load_file('link_dictionary.yml')
+  default_link = link_dictionary['DEFAULT'].to_s
+  new_link = link_dictionary[code].to_s
+  return new_link if new_link.length > 0
+  return default_link if default_link.length > 0
+  return FINAL_DEFAULT_URL
 end
