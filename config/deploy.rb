19b4c5ea5e1672cf87e519ab49ee23f06eca7ca6
XERAEN <x@XERAEN.net>
2022-07-13 22:17:55 -0500

    Adds puma env config

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,3 +28,4 @@ set :ssh_options, :port => ENV['SSH_PORT'] || 22, :forward_agent => true
 
 set :asdf_tools, %w{ ruby }
 set :asdf_map_ruby_bins, %w{ bundle gem rake ruby }
+set :puma_service_unit_env_file, '/home/xeraen/.cap_env'
