6c075a213e45f11deff23cc003d7f2408c58fab4
XERAEN <x@XERAEN.net>
2022-07-13 22:28:44 -0500

    Sets default env for cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,7 @@ append :linked_files, 'config/link_dictionary.yml'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets'
 
 # Default value for default_env is {}
-# set :default_env, { path: '/opt/ruby/bin:$PATH' }
+set :default_env, { :path => '/home/xeraen/.asdf/bin:/home/xeraen/.asdf/shims:$PATH' }
 
 # Default value for local_user is ENV['USER']
 # set :local_user, -> { `git config user.name`.chomp }
@@ -27,4 +27,4 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets'
 set :ssh_options, :port => ENV['SSH_PORT'] || 22, :forward_agent => true
 
 set :asdf_tools, %w{ ruby }
-append :asdf_map_ruby_bins, %w{ bundle gem rake ruby puma pumactl }
+set :asdf_map_ruby_bins, %w{ bundle gem rake ruby puma pumactl }
